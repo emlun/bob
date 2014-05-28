ffe65127021a139978e35cd7aec6c9e1727ecbe1
Emil Lundberg <lundberg.emil@gmail.com>
2014-05-28 22:02:37 +0200

    Add comments

--- backward_output_bf.py
+++ backward_output_bf.py
@@ -10,6 +10,7 @@ width = 80
 if len(sys.argv) > 1:
     width = int(sys.argv[1])
 
+# Utility function for printing neatly aligned lines
 output_lines = ['']
 def output(s):
     output_lines[-1] += s
@@ -18,11 +19,14 @@ def output(s):
         output_lines[-2] = output_lines[-2][:width]
 
 
+# Read STDIN into a list of character codes
 desired_output_code_sequence = []
 for line in sys.stdin:
     for c in line:
         desired_output_code_sequence.append(ord(c))
 
+
+# Pre-initialize registers with character codes
 output_language = list(set(desired_output_code_sequence))
 output_language.sort()
 
@@ -35,6 +39,7 @@ for i in range(0, len(output_language)):
 output('<')
 
 
+# Print the registers' values in the order specified by STDIN
 current_register = len(output_language)-1
 for i in desired_output_code_sequence:
     while i < output_language[current_register]:
@@ -45,5 +50,7 @@ for i in desired_output_code_sequence:
         current_register += 1
     output('.')
 
+
+# Actually print stuff
 for line in output_lines:
     print(line)
